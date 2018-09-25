36de0bdb2ce1b80c34ad87c57bd220452710b3c3
Cédric Félizard <cedric@felizard.fr>
2018-09-25 13:43:22 +0000

    Update Fluent Bit to latest version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,12 @@
-default['fluentbit']['version'] = '0.13.4'
-default['fluentbit']['checksum'] = '0b18399c2fff97ecc2ba7164708da4bba10da58c362e627e26885900d9f7f030'
+default['fluentbit']['version'] = '0.14.3'
+default['fluentbit']['checksum'] = '57f3e68dcea33ed4233ab2a9ad104e0a4a8aaae81024ecc95d3319c20b767dba'
 default['fluentbit']['archive'] = "fluent-bit-#{default['fluentbit']['version']}.tar.gz"
 default['fluentbit']['url'] = "http://fluentbit.io/releases/#{default['fluentbit']['version'].split('.')[0..-2].join('.')}/#{default['fluentbit']['archive']}"
 
 default['fluentbit']['dependencies'] = %w(make cmake g++)
 default['fluentbit']['dependencies'] << 'libsystemd-dev' # required for systemd input plugin
 default['fluentbit']['uninstall_dependencies'] = true # clean up deps after installation?
-default['fluentbit']['make_flags'] = '-j4'
+default['fluentbit']['make_flags'] = '-j $(nproc)'
 
 default['fluentbit']['install_dir'] = '/usr/local/bin'
 default['fluentbit']['conf_dir'] = '/etc/fluent-bit'
