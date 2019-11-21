fa0b1a1fa3d6ce6b7ac688be1186f084930008b0
Cédric Félizard <cedric@felizard.fr>
2019-11-21 15:03:38 +0000

    Update Fluent Bit to latest version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 # XXX: see https://fluentbit.io/download/ for latest version
-default['fluentbit']['version'] = '1.1.3'
-default['fluentbit']['checksum'] = '93d9b66ca8809d249c2ddffc2b8a6c4f86442721b9104c1157b869584c319c55'
+default['fluentbit']['version'] = '1.3.2'
+default['fluentbit']['checksum'] = '12684fd77ff8ac98d9d66c1840761a250118bc5305b5496f6f8f9110478beb95'
 default['fluentbit']['archive'] = "fluent-bit-#{default['fluentbit']['version']}.tar.gz"
 default['fluentbit']['url'] = "https://fluentbit.io/releases/#{default['fluentbit']['version'].split('.')[0..-2].join('.')}/#{default['fluentbit']['archive']}"
 
-default['fluentbit']['dependencies'] = %w(make cmake g++)
+default['fluentbit']['dependencies'] = %w(make cmake g++ pkg-config bison flex)
 default['fluentbit']['dependencies'] << 'libsystemd-dev' # required for systemd input plugin
 default['fluentbit']['uninstall_dependencies'] = true # clean up deps after installation?
 default['fluentbit']['make_flags'] = '-j $(nproc)'
